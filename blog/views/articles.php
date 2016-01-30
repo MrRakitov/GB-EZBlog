237d2db6c7f2d7bacf75a85312f13cdcf6cec9bf
Stanislav Rakitov <rakitov.s@gmail.com>
2016-01-30 21:37:31 +0300

    Link to admin panel added

--- blog/views/articles.php
+++ blog/views/articles.php
@@ -2,13 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Мой первый блог</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://yastatic.net/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
         <h1>Мой первый блог</h1>
+        <a href="admin">Панель администратора</a>
         <div>
             <?php foreach($articles as $a): ?>
             <div class="article">

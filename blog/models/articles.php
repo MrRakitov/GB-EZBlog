2aea136b703caaf78bdba43d61dc778a928a695b
Stanislav Rakitov <rakitov.s@gmail.com>
2016-01-30 17:48:12 +0300

    Models added

---
+++ blog/models/articles.php
@@ -0,0 +1,36 @@
+<?php
+
+//View all articles
+//
+function articles_all () {
+
+}
+
+//Get article with specific id
+//
+function articles_get ($id) {
+
+}
+
+//Create a new article
+//
+function articles_new ($title, $date, $content) {
+
+}
+
+//
+//
+function articles_edit ($id, $title, $date, $content) {
+
+}
+
+//Delete article with specific id
+//
+function articles_delete ($id) {
+
+}
+
+
+
+
+?>

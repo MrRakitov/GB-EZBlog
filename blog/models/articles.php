409428b13ddf24620780aedcfe19a3f7530785e9
Stanislav Rakitov <rakitov.s@gmail.com>
2016-01-30 18:17:45 +0300

    Models edit

--- blog/models/articles.php
+++ blog/models/articles.php
@@ -3,12 +3,21 @@
 //View all articles
 //
 function articles_all () {
+    $art1 = ["id"=>1, "title"=>"Title 1", "date"=>"2016-01-30", "content"=>"Content 1"];
+    $art2 = ["id"=>2, "title"=>"Title 2", "date"=>"2016-01-30", "content"=>"Content 2"];
+
+    $arr[0] = $art1;
+    $arr[1] = $art2;
+
+    return $arr;
 
 }
 
 //Get article with specific id
 //
 function articles_get ($id) {
+
+    return ["id"=>1, "title"=>"It is a simple title", "date"=>"2016-01-30", "content"=>"Here will be an article"];
 
 }
 

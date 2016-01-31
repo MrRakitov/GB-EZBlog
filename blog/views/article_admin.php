ee6c63ac411115275ca2de88b6477376af475fb2
Stanislav Rakitov <rakitov.s@gmail.com>
2016-01-31 15:35:14 +0300

    Articles edition added

--- blog/views/article_admin.php
+++ blog/views/article_admin.php
@@ -10,17 +10,17 @@
     <div class="container">
        <h1>Мой первый блог</h1>
         <div>
-        <form action="index.php?action=add" method="post">
+        <form action="index.php?action=<?=$_GET['action']?>&id=<?=$_GET['id']?>" method="post">
             <label>Название<br>
-                <input type="text" name="title" value="" class="form-item" autofocus required>
+                <input type="text" name="title" value="<?=$article['title']?>" class="form-item" autofocus required>
             </label>
             <br>
             <label>Дата<br>
-                <input type="date" name="date" value="" class="form-item" required>
+                <input type="date" name="date" value="<?=$article['date']?>" class="form-item" required>
             </label>
             <br>
             <label>Содержимое<br>
-                <textarea class="form-item" name="content" required></textarea>
+                <textarea class="form-item" name="content" required><?=$article['content']?></textarea>
             </label><br>
             <input type="submit" value="Сохранить" class="btn">
         </form>
